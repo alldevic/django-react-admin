16c7f6e1c7a4ff8881a66e18287190b1a9a1b7d3
Nikolay Bely <belyynikolay@gmail.com>
2021-09-05 22:29:13 +0700

    Remove iframe, add inner html

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,17 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import DebugFrame from './DebugFrame';
 
 ReactDOM.render(
-
   <React.StrictMode>
-    <DebugFrame />
     <App />
-  </React.StrictMode>
-  ,
+  </React.StrictMode>,
   document.getElementById('root')
 );
+
+fetch("http://localhost:8000/debug/")
+  .then((response) => response.text())
+  .then(data => {
+    data = data.replace(
+      'data-render-panel-url="/__debug__/render_panel/"',
+      'data-render-panel-url="http://localhost:8000/__debug__/render_panel/"'
+    )
+    const djdt = document.createRange().createContextualFragment(data)
+    document.getElementById("root").appendChild(document.importNode(djdt, true));
+  });
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
